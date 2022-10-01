754ebef05792c5c2f221296ff84e733317a47c3a
th1enmenh <ldphong99@gmail.com>
2022-10-01 17:30:36 +0700

    okkook

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+// module import
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000
 const path = require("path")
 
+// database connect +access
 const mongoose = require("mongoose")
 const studentModel = require("./models/StudentSchema")
 const url = "mongodb://localhost:27017/greenwich"
@@ -13,6 +15,44 @@ mongoose.connect(url, {useNewUrlParser: true}, (err)=> {
         console.log("connect to db success !")
     }
 })
+// body-parser: get input form FORM
+const bodyParser = require("body-parser")
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.get("/login", (req,res)=>{
+    res.render("login")
+})
+app.post("/login",(req,res)=>{
+    var username = req.body.username
+    var password = req.body.password
+    var check = "login failed!";
+    if(username == "admin" && password == "123456") {
+        check = "login succed!"
+    }
+    res.render("check", {result: check})
+})
+app.get("/add", (req,res) =>{
+    res.render("add")
+})
+app.post("/add", (req,res)=>{
+    //res.render("output", {student: req.body})
+    
+// luu object student chua du lieu nhap tu form
+    var student = new studentModel({
+        name: req.body.name,
+        age: req.body.age,
+        email: req.body.email,
+        image: req.body.image,
+    })
+    student.save((err) => {
+        if(err) {
+            console.error(err)
+        }else {
+            res.redirect("/student")
+        }
+    })
+})
+
 
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "hbs")
